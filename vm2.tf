ebcbfaea90dd35e24d3eb42f78705e76407ba9be
BetaVersionAnurag <anuragjain.cse@gmail.com>
2021-07-09 22:16:10 +0530

    Update vm2.tf

--- vm2.tf
+++ vm2.tf
@@ -1,24 +1,24 @@
-resource "azurerm_network_interface" "example-2" {
-  name                = "example-2-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "maersk-2" {
+  name                = "maersk-2-nic"
+  location            = azurerm_resource_group.maersk.location
+  resource_group_name = azurerm_resource_group.maersk.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example-2.id
+    subnet_id                     = azurerm_subnet.maersk-2.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_windows_virtual_machine" "example-2" {
-  name                = "example-2-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_windows_virtual_machine" "maersk-2" {
+  name                = "maersk-2-machine"
+  resource_group_name = azurerm_resource_group.maersk.name
+  location            = azurerm_resource_group.maersk.location
   size                = "Standa rd_F2"
   admin_username      = "adminuser"
   admin_password      = data.azurerm_key_vault_secret.admin-password.value
   network_interface_ids = [
-    azurerm_network_interface.example-2.id,
+    azurerm_network_interface.maersk-2.id,
   ]
 
   os_disk {
