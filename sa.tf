9eba9d90dbbfda92939595b0e31e3433254ef38b
BetaVersionAnurag <anuragjain.cse@gmail.com>
2021-07-09 22:03:01 +0530

    Update sa.tf

--- sa.tf
+++ sa.tf
@@ -1,7 +1,7 @@
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "maersk" {
   name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.maersk.name
+  location                 = azurerm_resource_group.maersk.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
